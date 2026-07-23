2113d4a0f2757091b7abf4e49a411e5b918a87d3
ruka76046-commits <ruka76046@gmail.com>
2026-07-23 15:56:19 +0900

    chore: save local changes

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,14 @@
-// vite.config.js
-import { defineConfig } from 'vite';
-import { resolve } from 'path';
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
-  base: '/my-website/', // ← ここを '/my-website/' に設定します！
+  base: '/my-website/',
   build: {
     rollupOptions: {
       input: {
-        main: resolve(import.meta.dirname, 'index.html'),
-        app: resolve(import.meta.dirname, 'app.html'),
+        main: resolve(__dirname, 'index.html'),
+        app: resolve(__dirname, 'app.html'),
       },
     },
   },
-});
+})
